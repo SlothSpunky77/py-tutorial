3ee8daf9b86b658590b50f83b7e110301b8b4702
SlothSpunky77 <94778190+SlothSpunky77@users.noreply.github.com>
2022-09-16 15:30:05 +0530

    Add files via upload

--- binary_search.py
+++ binary_search.py
@@ -1,9 +1,20 @@
+
+beg = 0;
+end = 7;
+mid = int((beg + end)/2)
 a = [1,2,3,4,5,6,7,8]
 print(a)    
-n = input("Enter a number from 1 to 8: ")
-
-if (n >= 1 and n <= 8):
-
-    
-
-    
+n = int(input("Enter a number from 1 to 8: "))
+p = n - 1
+c = 1000
+if (p >= 0 and p <= 7):
+    while (c != mid):
+        if (p < mid):
+            end = mid;
+        elif (p > mid):
+            beg = mid;
+        else:
+            c = mid;
+else:
+    print("Invalid number. Idiot.")
+print("The position is: ", c)
